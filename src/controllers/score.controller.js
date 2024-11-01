367203985a8a7c62a59118cf519db51dd692732a
Gustavo Fernandes <gutocfernandes@hotmail.com>
2024-11-01 12:41:54 -0300

    feat: add period filter to default scores endpoint

--- src/controllers/score.controller.js
+++ src/controllers/score.controller.js
@@ -1,7 +1,12 @@
 import { getScoresColl } from '../database.js'
+import { getUnixTime } from 'date-fns'
 
 /**
  * Retrieves all scores from the 'scores' collection in the MongoDB database
+ * 
+ * @query period : number - Amount of days to look back / default: 365 (1 year)
+ * @query limit : number - Limit the amount of scores returned (1 ~ x) / default: 500
+ * @query invert : string - true: inverts the order / default: sort scores from highest to lowest
  * 
  * @param req 
  * @param res 
@@ -9,11 +14,11 @@ import { getScoresColl } from '../database.js'
 async function getAllScores(req, res) {
     const coll = await getScoresColl()
 
-    const { limit, invert } = req.query
+    const { limit, invert, period } = req.query
 
     // Set the limit using a parameter, defaulting to 500 if none is provided
-    const parsedLimit = (()=>{
-        if(limit){
+    const parsedLimit = (() => {
+        if (limit) {
             const parsedValue = parseInt(limit)
 
             if (!isNaN(parsedValue)) {
@@ -23,7 +28,25 @@ async function getAllScores(req, res) {
         return 500
     })()
 
-    const scores = await coll.find({}).limit(parsedLimit).sort({ score: invert === 'true' ? 1 : -1 }).toArray()
+    // Define the period in days for filtering scores
+    const parsedPeriod = (() => {
+        if (period) {
+            const parsedValue = parseInt(period)
+
+            if (!isNaN(parsedValue)) {
+                return Math.max(1, parsedValue) * 60 * 60 * 24 // Convert to timestamp
+            }
+        }
+        return 31536000 // Default to 1 year in seconds
+    })()
+
+    // Fetch the scores from the database
+    const scores = await coll
+        .find({ timestamp: { $gte: getUnixTime(new Date()) - parsedPeriod } })
+        .limit(parsedLimit)
+        .sort({ score: invert === 'true' ? 1 : -1 })
+        .toArray()
+
     res.status(200).json(scores)
 }
 
