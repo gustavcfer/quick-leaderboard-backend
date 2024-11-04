4c1d2795d2e4d684a57d8c16e78e8d81f9fb6a48
Gustavo Fernandes <gutocfernandes@hotmail.com>
2024-11-04 14:32:27 -0300

    docs: update readme
    Update the Endpoint on the README.md file, as well as the installation
    guide. Also changed some minor things in authentication.

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -6,7 +6,7 @@ import 'dotenv/config'
 
 
 /**
- * Basic authorization security with API KEY
+ * Basic authorization security with apikey / hash
  * 
  * @param req 
  * @param res 
@@ -14,19 +14,22 @@ import 'dotenv/config'
  * @returns *void* if unauthorized
  */
 export function auth(req, res, next) {
-    const apiKey = 'Bearer ' + process.env.API_SECRET
 
     const token = req.headers.authorization
 
-    if(!token){
+    if (!token) {
         logger.error('Request with no TOKEN in the HEADER was rejected')
-        res.status(401).json({error: 'No token'})
+        res.status(401).json({ error: 'No token' })
         return
     }
 
-    if(token !== apiKey){
+    const hash = 'Bearer ' + crypto
+        .createHmac('sha256', process.env.API_SECRET)
+        .digest('hex')
+
+    if (token !== hash) {
         logger.error('Request with INVALID TOKEN was rejected')
-        res.status(401).json({error: 'Invalid token'})
+        res.status(401).json({ error: 'Invalid token' })
         return
     }
 
@@ -46,17 +49,15 @@ export function putAuth(req, res, next) {
 
     const token = req.headers.authorization
 
-    if(!token){
-        logger.error('Request with no TOKEN in the HEADER was rejected')
-        res.status(401).json({error: 'No token'})
+    if (!token) {
+        res.status(401).json({ error: 'No token' })
         return
     }
 
-    const {nickname, key, score, timestamp } = req.body
+    const { nickname, key, score, timestamp } = req.body
 
-    if(!key || !score){
-        logger.error('Request body without KEY or SCORE was rejected')
-        res.status(400).json({error: 'Body should have KEY and SCORE to be a valid request'})
+    if (!key || !score) {
+        res.status(400).json({ error: 'Body should have KEY and SCORE to be a valid request' })
         return
     }
 
@@ -65,9 +66,8 @@ export function putAuth(req, res, next) {
         .update(key)
         .digest('hex')
 
-    if(token !== hash){
-        logger.error('Request with INVALID TOKEN was rejected')
-        res.status(401).json({error: 'Invalid token'})
+    if (token !== hash) {
+        res.status(401).json({ error: 'Invalid token' })
         return
     }
 
